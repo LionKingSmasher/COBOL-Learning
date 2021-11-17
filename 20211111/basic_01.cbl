bacc4e628a54b974af6e18ab95d74231bd8d2f0d
Shin-Hyun-Kyu <shk052353@gmail.com>
2021-11-17 22:20:20 +0900

    Today My Learning 20211117

--- 20211111/basic_01.cbl
+++ 20211111/basic_01.cbl
@@ -1,14 +1,15 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. BASIC_01.
-
-DATA DIVISION.
-	WORKING-STORAGE SECTION.
-	01 WS-STUDENT-NAME PIC X(25). *> Value : X(Alphanumeric) 25
-	01 WS-DATE PIC X(10). *> Value : X(Alphanumeric) 10
-
-PROCEDURE DIVISION.
-	ACCEPT WS-STUDENT-NAME.   *> Value get value by user
-	ACCEPT WS-DATE FROM DATE. *> Value get value by Date
-	DISPLAY "Name : ", WS-STUDENT-NAME.
-	DISPLAY "Date : ", WS-DATE.
-STOP RUN.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BASIC_01.
+       
+       
+       DATA DIVISION.
+       	WORKING-STORAGE SECTION.
+       	01 WS-STUDENT-NAME PIC X(25). *> Value : X(Alphanumeric) 25
+       	01 WS-DATE PIC X(10). *> Value : X(Alphanumeric) 10
+       
+       PROCEDURE DIVISION.
+       	ACCEPT WS-STUDENT-NAME.   *> Value get value by user
+       	ACCEPT WS-DATE FROM DATE. *> Value get value by Date
+       	DISPLAY "Name : ", WS-STUDENT-NAME.
+       	DISPLAY "Date : ", WS-DATE.
+       STOP RUN.
---
+++ 20211117/move.cbl
@@ -0,0 +1,31 @@
+      ******************************************************************
+      *                                                                *
+      *                                                                *
+      *                           Move Verb                            *
+      *                                                                *
+      *    Date: 2021-11-17                                            *
+      *    Description: Today My Learning                              *
+      *    Author: Shin Hyun Kyu                                       *
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MOVE_VERB.
+
+       DATA DIVISION.
+           WORKING-STORAGE SECTION.
+               01 DB-NAME PIC A(20) VALUE 'Shin Hyun Kyu'.
+               01 DB-ID PIC X(13) VALUE 'A1B2C3D4E5F6'.
+               01 DB-HOME.
+                   05 DB-HOME-CITY PIC A(25).
+                   05 DB-HOME-TOWN PIC A(25).
+       
+       PROCEDURE DIVISION.
+           FIRST-PARA.
+           ACCEPT DB-HOME-CITY.
+           MOVE DB-HOME-CITY TO DB-HOME-TOWN.
+           DISPLAY "Your Name:", DB-NAME.
+           DISPLAY "Your ID:", DB-ID.
+           DISPLAY "Your Home:", DB-HOME.
+       STOP RUN.
